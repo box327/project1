bc81d278b3d97567b53c0783ff9267a556623fe5
dongyoung kim <kibox327@gmail.com>
2017-01-18 16:10:12 +0900

    update function complete and nav modify

--- src/main/java/test/test/controller/UserController.java
+++ src/main/java/test/test/controller/UserController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +20,7 @@ public class UserController {
 	
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-	private List<UserData> userList = new ArrayList<UserData>();
+	private ArrayList<UserData> userList = new ArrayList<UserData>();
 
 	@PostMapping("/create")
 	public String createUser(UserData userData)
@@ -35,4 +36,26 @@ public class UserController {
 		model.addAttribute("userList", userList);
 		return "user/list";
 	}
+	
+	@GetMapping("/{id}/form")
+	public String updadeForm(Model model, @PathVariable(value="id")Integer id)
+	{
+		log.debug(id.toString());
+		UserData user = userList.get(id - 1);
+		model.addAttribute("user", user);
+		log.debug(user.toString());
+		return "user/form";
+	}
+	
+	@PostMapping("/update")
+	public String update(Model model,UserData userData ,Integer id)
+	{
+		UserData user = userList.get(id - 1);
+		user.setEmail(userData.getEmail());
+		user.setName(userData.getName());
+		user.setUserId(userData.getUserId());
+		user.setPassword(userData.getPassword());
+		return "redirect:/user/list";
+	}
+	
 }
